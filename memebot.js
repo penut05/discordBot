7c2b750802f2bbb4ad3215167d8bd28424f71305
Pakoola <pakoola@aol.com>
2017-01-15 11:24:56 -0600

    Clean up Time...

--- memebot.js
+++ memebot.js
@@ -3,27 +3,17 @@
 //** Local Directory path /home/justin/discordBot **
 //**
 // ****************************************
-//New ES6 Node/Javascript stuff
-'use strict'
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Discord = require("discord.js");
-const bot = new Discord.Client();
-const request = require("request");
-
-bot.login("MjcwMjM5NDM2OTgwMjI0MDAw.C11ALQ.3oWQPghx-HumtWRJEauE8s8rH2M");
-
-
-//when the bot is ready
-bot.on('ready', () => {
-  console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+client.on('ready', () => {
+  console.log('I am ready!');
 });
 
-//when the bot disconnects
-bot.on("disconnected", () => {
-    console.log("Disconnected, exiting!");
-    process.exit();
+client.on('message', msg => {
+    if (msg.content === '!test') {
+        message.reply('Fk Shimko');
+    }
 });
 
-bot.on('message', msg => {
-    console.log("Testing new versions");
-});
+client.login("MjcwMjM5NDM2OTgwMjI0MDAw.C11ALQ.3oWQPghx-HumtWRJEauE8s8rH2M");
